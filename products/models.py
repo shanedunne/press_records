8adb2d62f126ac89ce68a22a6eab52e1f33abab8
shanedunne <shanejjdunne@hotmail.com>
2021-10-19 08:22:18 +0000

    add artist to product model

--- products/models.py
+++ products/models.py
@@ -16,7 +16,8 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
+    album_name = models.CharField(max_length=254)
+    artist_name = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     genre = models.CharField(max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
