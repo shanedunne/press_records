887795fd4a70df7a619afa9d8bb149173f01485d
shanedunne <shanejjdunne@hotmail.com>
2021-10-21 18:19:04 +0000

    add bag table and functionality to add to bag

--- bag/views.py
+++ bag/views.py
@@ -9,6 +9,7 @@ def view_bag(request):
     """
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
@@ -22,5 +23,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
