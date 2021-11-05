f42fc39463d41cdbdfe0b936975cd931807400b8
shanedunne <shanejjdunne@hotmail.com>
2021-11-05 14:11:20 +0000

    fix route to all_blogs and blog_post

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
-    path('<int:blog_id>/', views.blog_post, name='blog_post'),
-    # path('add/', views.add_product, name='add_product'),
-    # path('edit<int:product_id>/', views.edit_product, name='edit_product'),
-    # path('delete<int:product_id>/', views.delete_product, name='delete_product'),
+    path('<blog_id>/', views.blog_post, name='blog_post'),
 
 ]
