8327127a357d37da2d0e2bb3e9121b807ed0afb0
shanedunne <shanejjdunne@hotmail.com>
2021-11-11 20:01:19 +0000

    add hifi deals slider to homepage

--- home/views.py
+++ home/views.py
@@ -10,9 +10,11 @@ def index(request):
 
     """
     is_feature = Product.objects.filter(is_feature=True)
+    is_hifi_deal = Product.objects.filter(is_hifi_deal=True)
 
     context = {
         'is_feature': is_feature,
+        'is_hifi_deal': is_hifi_deal,
     }
 
     return render(request, 'home/index.html', context)
