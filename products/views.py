affdd4a5b10f414642f9e07da6d2ea1932b0dc51
shanedunne <shanejjdunne@hotmail.com>
2021-11-12 21:41:41 +0000

    made fix to sort box

--- products/views.py
+++ products/views.py
@@ -33,8 +33,6 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-            if sortkey == 'artist_name':
-                sortkey = 'artist_name__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
