ddc0baa46b5c9233fecd17613a0a337a3e7bb70f
shanedunne <shanejjdunne@hotmail.com>
2021-11-19 17:44:07 +0000

    add tests for profile views

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import UserProfile
+from checkout.models import Order
 
-# Create your tests here.
+class TestUserProfileViews(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@test.com',
+            password='testpassword'
+        )
+        self.profile = reverse('profile')
+    
+
+    def test_get_profile_view(self):
+        """
+        A test to get the profile view
+        """
+        self.client.login(
+            username="testuser", password="testpassword")
+        response = self.client.get(self.profile)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/profile.html')
+    
+
+    def test_get_order_history(self):
+        """
+        A test to get the order history on the profile page
+        """
+        self.client.login(
+            username="testuser", password="testpassword")
+        user_profile = UserProfile.objects.get(user=self.user)
+        order = Order.objects.create(user_profile=user_profile)
+        response = self.client.get(reverse('order_history', 
+                                    kwargs={"order_number": order.order_number}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'checkout/checkout_success.html')
