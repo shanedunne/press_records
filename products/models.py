b80e076d42cdb12988b416c7b56ebabdbc02fe94
shanedunne <shanejjdunne@hotmail.com>
2021-11-20 09:17:35 +0000

    set up emails

--- products/models.py
+++ products/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Category(models.Model):
 
@@ -18,7 +17,9 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category',
+                                    null=True, blank=True,
+                                    on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     artist_name = models.ForeignKey('Artist', null=True, blank=True, on_delete=models.SET_NULL)
@@ -51,4 +52,3 @@ class Artist(models.Model):
 
     def __str__(self):
         return self.name
-
